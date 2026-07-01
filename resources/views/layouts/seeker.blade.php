501e19d571ac771924a83c6208071b25a78faf3c
Herbatech Dev <dev@herbatech.com>
2026-07-01 09:09:25 +0700

    fix: add favicon to seeker layout

--- resources/views/layouts/seeker.blade.php
+++ resources/views/layouts/seeker.blade.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $siteSettings?->company_name ?? 'HerbaTech' }} - Career Portal</title>
 
+    @if(isset($siteSettings) && $siteSettings->favicon)
+        <link rel="icon" href="{{ asset('storage/' . $siteSettings->favicon) }}" type="image/x-icon"/>
+    @else
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+    @endif
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
